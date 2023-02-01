4f3ac651232dcb4a51a1661a2bba87754101a9c4
Harsh Tenguriya <32489875+TengoCharlie@users.noreply.github.com>
2023-02-01 15:13:44 +0000

    Input and output

--- index.cpp
+++ index.cpp
@@ -1,7 +1,15 @@
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int32_t main()
 {
-    cout << "Hello World!\n";
+    int amount1;
+    cin >> amount1;
+
+    int amount2;
+    cin >> amount2;
+    int sum = amount1 + amount2;
+    cout << sum << endl;
+    return 0;
 }
