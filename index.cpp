343c0403add902aa71c553809b4b2aa921a20df0
Harsh Tenguriya <32489875+TengoCharlie@users.noreply.github.com>
2023-02-01 15:19:22 +0000

    Beautify code

--- index.cpp
+++ index.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int32_t main()
 {
+
+    // This part is to beautify the input and output of the code
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
     int amount1;
     cin >> amount1;
 
